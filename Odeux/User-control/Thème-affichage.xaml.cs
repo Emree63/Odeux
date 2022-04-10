5792b127d9503f59147fde185371cd9bc1ea7a2b
Emre <Emre.kartal@etu.uca.fr>
2022-04-10 17:56:08 +0200

    Maj

---
+++ Odeux/User-control/Thème-affichage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Odeux
+{
+    /// <summary>
+    /// Logique d'interaction pour Thème_affichage.xaml
+    /// </summary>
+    public partial class Thème_affichage : UserControl
+    {
+        public Thème_affichage()
+        {
+            InitializeComponent();
+        }
+    }
+}
