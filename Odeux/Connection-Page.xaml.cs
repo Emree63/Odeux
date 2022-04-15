d48fd402814eba634677a51bef255d97456c3e7e
Emre KARTAL <Emre.KARTAL@etu.uca.fr>
2022-04-15 11:58:40 +0200

    Quelque ajout WindowState: pour regler la taille automatiquement Pop up: Lisez vos mails et regle des dimensions des images de la page connexion

--- Odeux/Connection-Page.xaml.cs
+++ Odeux/Connection-Page.xaml.cs
@@ -31,6 +31,7 @@ namespace Odeux
             Accueil win = new Accueil();
             win.Show();
             this.Close();
+            MessageBox.Show("C'est important et ça fait gagner du temps et de l'énergie à tout le monde !","Lisez vos mails tous les jours !", MessageBoxButton.OK, MessageBoxImage.Information,MessageBoxResult.OK);
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
