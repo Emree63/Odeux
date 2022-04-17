16b942c3da76088124633ba5a778ac98885e0f56
Emre KARTAL <Emre.KARTAL@etu.uca.fr>
2022-04-18 01:53:33 +0200

    Ajout des couleurs de la charte graphique

--- Odeux/User-control/button_Thème-Affichage.xaml.cs
+++ Odeux/User-control/button_Thème-Affichage.xaml.cs
@@ -33,6 +33,8 @@ namespace Odeux.User_Control
             Sombre.Height = 0;
             Oled.Height = 0;
             Application.Current.Resources["BackgroundColor"] = new SolidColorBrush(Color.FromRgb(235, 249, 254));
+            Application.Current.Resources["BackgroundColor2"] = new SolidColorBrush(Color.FromRgb(198, 238, 253));
+            Application.Current.Resources["TextColor"] = new SolidColorBrush(Color.FromRgb(2, 27, 34));
         }
         private void Sombre_button(object sender, RoutedEventArgs e)
         {
@@ -40,6 +42,8 @@ namespace Odeux.User_Control
             Sombre.Height = 22;
             Oled.Height = 0;
             Application.Current.Resources["BackgroundColor"] = new SolidColorBrush(Color.FromRgb(3, 50, 67));
+            Application.Current.Resources["BackgroundColor2"] = new SolidColorBrush(Color.FromRgb(6, 106, 145));
+            Application.Current.Resources["TextColor"] = new SolidColorBrush(Color.FromRgb(241, 251, 254));
         }
         private void Oled_button(object sender, RoutedEventArgs e)
         {
@@ -47,6 +51,8 @@ namespace Odeux.User_Control
             Sombre.Height = 0;
             Oled.Height = 22;
             Application.Current.Resources["BackgroundColor"] = new SolidColorBrush(Color.FromRgb(0,0,0));
+            Application.Current.Resources["BackgroundColor2"] = new SolidColorBrush(Color.FromRgb(3, 44, 58));
+            Application.Current.Resources["TextColor"] = new SolidColorBrush(Color.FromRgb(241, 251, 254));
         }
 
     }
