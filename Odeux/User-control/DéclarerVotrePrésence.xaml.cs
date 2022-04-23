fedcebbb1a8c0e9b2b11f22ad88d82a777dd1546
Emre KARTAL <Emre.KARTAL@etu.uca.fr>
2022-04-23 11:56:10 +0000

    Toujour une Sauvegarde rien de concret

--- Odeux/User-control/DéclarerVotrePrésence.xaml.cs
+++ Odeux/User-control/DéclarerVotrePrésence.xaml.cs
@@ -28,6 +28,8 @@ namespace Odeux.User_Control
             {
                 new Presence{dateCour = new DateTime(2022,03,25), Matiere = "R2.01 : Math", groupe=3, Salle="A13", Intervenant = "Marc"},
                 new Presence{dateCour = new DateTime(2022,03,26), Matiere = "R2.04 : Anglais", groupe=3, Salle="B18", Intervenant = "Astier"},
+                new Presence{dateCour = new DateTime(2022,03,25), Matiere = "R2.01 : Math", groupe=3, Salle="A13", Intervenant = "Marc"},
+                new Presence{dateCour = new DateTime(2022,03,26), Matiere = "R2.04 : Anglais", groupe=3, Salle="B18", Intervenant = "Astier"},
             };
 
             ListBox.ItemsSource = LesPresence;
