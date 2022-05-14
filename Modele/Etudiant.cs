26201eacbb423d8d15eb87dc66afc1aace5fe6e4
Emre KARTAL <Emre.KARTAL@etu.uca.fr>
2022-05-14 09:41:02 +0200

    Amélioration C# 70% fait

--- Modele/Etudiant.cs
+++ Modele/Etudiant.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Modele
 {
-    class Etudiant : Personne
+    public class Etudiant : Personne
     {
+        /// <summary>
+        /// Premier semestre de l'étudiant
+        /// </summary>
+        private Semestre Semestre1;
+        public Semestre semestre1 { get => Semestre1; set => Semestre1 = value; }
+
+        /// <summary>
+        /// Deuxième semestre de l'étudiant
+        /// </summary>
+        private Semestre Semestre2;
+        public Semestre semestre2 { get => Semestre2; set => Semestre2 = value; }
+
         /// <summary>
         /// Constructeur
         /// </summary>
@@ -15,9 +27,12 @@ namespace Modele
         /// <param name="prénom">prénom de l'étudiant</param>
         /// <param name="naissance">date de naissance de l'étudiant</param>
         /// <param name="password">Mot de passe(privé) pour se connecter</param>
-        public Etudiant(string nom, string prénom, DateTime naissance, string password) : base(nom,prénom,naissance,password)
+        /// <param name="s1">Premier Semestre de l'etudiant</param>
+        /// <param name="s2">Deuxième Semestre de l'etudiant</param>
+        public Etudiant(string nom, string prénom, DateTime naissance, string password, Semestre s1, Semestre s2) : base(nom,prénom,naissance,password)
         {
-
+            Semestre1 = s1;
+            Semestre2 = s2;
         }
 
         public override string ToString()
@@ -25,5 +40,26 @@ namespace Modele
             return $"Je suis un étudiant et je m'appelle : {nom} {prénom}";
         }
 
+        /// <summary>
+        /// Moyenne General de l'eleve sur toute son année
+        /// </summary>
+        /// <returns>double : Moyenne</returns>
+        public double MoyGeneral()
+        {
+
+            double total = 0;
+
+            try
+            {
+                total = Semestre1.MoySemestre() + Semestre2.MoySemestre();
+            }
+            catch (Exception msg)
+            {
+                Console.WriteLine(msg);
+                return 0;
+            }
+            return total / 2;
+        }
     }
+    
 }
--- Test_Etudiant/Program.cs
+++ Test_Etudiant/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using static System.Console;
+using Modele;
+using System.Collections.Generic;
 
 namespace Test_Etudiant
 {
@@ -8,6 +10,79 @@ namespace Test_Etudiant
         static void Main(string[] args)
         {
             WriteLine("Test de la classe Etudiant :");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Test de la classe UE :");
+
+            // Creation des Liste de note
+            List<Note> notes = new List<Note>();
+            Note n1 = new Note("Tp noté", 10);
+            notes.Add(n1);
+
+            Note n2 = new Note("DS", 15);
+            notes.Add(n2);
+
+
+            List<Note> notes2 = new List<Note>();
+            Note n3 = new Note("Tp noté", 13);
+            notes2.Add(n3);
+
+            Note n4 = new Note("DS", 11);
+            notes2.Add(n4);
+
+            Note n5 = new Note("Situation 1 (sur 20.0)", 16);
+
+            Note n6 = new Note("Situation 2 (sur 20.0)", 11);
+
+            //Creation des Matières
+
+            List<Matiere> matieres = new List<Matiere>();
+            List<Matiere> matieres2 = new List<Matiere>();
+
+            Matiere mat1 = new Matiere("Structure de données", notes);
+            Matiere mat2 = new Matiere("Algorithme", notes2);
+
+            matieres.Add(mat1);
+            matieres.Add(mat2);
+            matieres2.Add(mat1);
+
+            //Creation des Ressource
+
+            Ressource res = new Ressource("R1.01", "Initiation au développement", 42, matieres);
+            Ressource res2 = new Ressource("R1.02", 12, matieres2);
+
+            //Creation d'une UE
+
+            List<UE> lesUE = new List<UE>();
+            List<UE> lesUE2 = new List<UE>();
+
+            UE ue = new UE(1, "UE 1", "Développer des applications informatiques simples");
+            UE ue2 = new UE(1, "UE 2", "Appréhender et construire des algorithmes");
+
+            ue.AddRessource(res);
+            ue.AddRessource(res2);
+            ue2.AddRessource(res);
+
+            lesUE.Add(ue);
+            lesUE2.Add(ue2);
+
+            //Creation SAE
+
+            SAE sae = new SAE("S1.01 : Implémentation d’un besoin client", n5, 40);
+            ue.AddSAE(sae);
+
+            SAE sae2 = new SAE("S1.02 : Comparaison d’approches algorithmiques", n6, 40);
+            ue2.AddSAE(sae2);
+
+
+            //Creation de deux Semestre
+
+            Semestre s1 = new Semestre(1, lesUE);
+            Semestre s2 = new Semestre(2, lesUE2);
+
+            //Creation d'un Joueur
+
+            Etudiant etu1 = new Etudiant("Khedair ", "Rami");
         }
     }
 }
