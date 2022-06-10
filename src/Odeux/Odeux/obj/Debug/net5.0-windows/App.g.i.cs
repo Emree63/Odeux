ce77697686eb01359bb24698446bac0027008238
Emre <Emre.kartal@etu.uca.fr>
2022-06-10 23:41:08 +0200

    Déploiement réussi

--- src/Odeux/Odeux/obj/Debug/net5.0-windows/App.g.i.cs
+++ src/Odeux/Odeux/obj/Debug/net5.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Odeux {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Odeux {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         public static void Main() {
             Odeux.App app = new Odeux.App();
             app.InitializeComponent();
